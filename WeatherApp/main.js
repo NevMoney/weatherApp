9e23bfd738eb1c9ff79965bb964e23c3adc2e07c
Nev <67838965+NevMoney@users.noreply.github.com>
2022-12-25 16:17:16 -0500

    remove unused function

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -2,13 +2,6 @@ import './style.css'
 import { getWeather } from './weather'
 import { ICON_MAP } from './iconMap'
 
-// getWeather(41.824, 71.4128, Intl.DateTimeFormat().resolvedOptions().timeZone)
-//   .then(renderWeather)
-//   .catch((e) => {
-//     console.error(e)
-//     alert('Error:' + e.message)
-//   })
-
 function renderWeather({ current, daily, hourly }) {
   // console.log(current, daily, hourly)
   renderCurrentWeather(current)
